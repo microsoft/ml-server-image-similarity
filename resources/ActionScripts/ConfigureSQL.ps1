524338c70493a9b61b22d3298313e82c0fba5e7d
Bob White <v-robwhi@microsoft.com>
2018-02-16 12:41:42 -0800

    reverted to bulk insert

--- resources/ActionScripts/ConfigureSQL.ps1
+++ resources/ActionScripts/ConfigureSQL.ps1
@@ -238,16 +238,17 @@ Write-Host -ForeGroundColor 'cyan' (" Import CSV File(s). This Should take about
 
 
 # upload csv files into SQL tables
-foreach ($dataFile in $dataList)
+#foreach ($dataFile in $dataList)
 {
-    $destination = $SolutionData + $dataFile + ".csv" 
-    $tableName = $DBName + ".dbo." + $dataFile
-    $tableSchema = $dataPath + "\" + $dataFile + ".xml"
-    $dataSet = Import-Csv $destination
- Write-Host -ForegroundColor 'cyan' ("         Loading $dataFile.csv into SQL Table") 
-    Write-SqlTableData -InputData $dataSet  -DatabaseName $dbName -Force -Passthru -SchemaName dbo -ServerInstance $ServerName -TableName $dataFile
+#     $destination = $SolutionData + $dataFile + ".csv" 
+#     $tableName = $DBName + ".dbo." + $dataFile
+#     $tableSchema = $dataPath + "\" + $dataFile + ".xml"
+#     $dataSet = Import-Csv $destination
+#  Write-Host -ForegroundColor 'cyan' ("         Loading $dataFile.csv into SQL Table") 
+#     Write-SqlTableData -InputData $dataSet  -DatabaseName $dbName -Force -Passthru -SchemaName dbo -ServerInstance $ServerName -TableName $dataFile
 
-    
+    $qry = "BULK INSERT Query_Images FROM 'C:\Solutions\ImageSimilarity\Data\query_images.csv'"
+    Invoke-Sqlcmd -ServerInstance LocalHost -Database $dbName -Query $qry -ConnectionTimeout  0 -QueryTimeout 0
  Write-Host -ForeGroundColor 'cyan' (" $datafile table loaded from CSV File(s).")
 }
 }
