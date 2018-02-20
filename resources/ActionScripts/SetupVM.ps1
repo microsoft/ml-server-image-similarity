d18bf30ead0232256b6e4778fe6cc8986ffe8f98
Bob White <v-robwhi@microsoft.com>
2018-02-20 11:37:39 -0800

    added link to website

--- resources/ActionScripts/SetupVM.ps1
+++ resources/ActionScripts/SetupVM.ps1
@@ -319,7 +319,8 @@ Write-Host " Powershell nap time is over"
 }
 
 ##Launch HelpURL 
-Start-Process "https://microsoft.github.io/$SolutionFullName/Typical.html"
+##Start-Process "https://microsoft.github.io/$SolutionFullName/Typical.html"
+Start-Process "https://microsoft.github.io/ml-server-image-similarity/"
 
 $endTime = Get-Date
 
