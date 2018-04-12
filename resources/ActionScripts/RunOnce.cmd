e9c796d52232ecfa118da41de8616632e4ac347e
Bob White (Zen3 Infosolutions America Inc) <v-robwhi@microsoft.com>
2018-04-12 12:55:59 -0700

    updating cmd file

--- resources/ActionScripts/RunOnce.cmd
+++ resources/ActionScripts/RunOnce.cmd
@@ -1,2 +1,1 @@
-PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%tmp%\load_data.txt" 2>&1 
-PowerShell C:\Solutions\ImageSimilarity\resources\ActionScripts\LoadImageData.ps1 >> "%tmp%\load_data.txt"  2>&1
+Powershell.exe -executionpolicy unrestricted -File  C:\Solutions\ImageSimilarity\resources\ActionScripts\LoadImageData.ps1
