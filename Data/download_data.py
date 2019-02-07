57e514025b705ab5b87d4178f4b4dea1fa61861d
Jeroen ter Heerdt <jterh@microsoft.com>
2019-02-07 14:19:47 -0800

    updating image download script

--- Data/download_data.py
+++ Data/download_data.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
-import urllib2
+import urllib.request
+from urllib.error import HTTPError, URLError
+import socket
+from socket import *
 
 def download_and_place(type, link, data_dir, index):
     print("Downloading: type: {0}, link: {1}, data_dir: {2}, index: {3}".format(type,link,data_dir,index))
@@ -12,7 +15,7 @@ def download_and_place(type, link, data_dir, index):
         print("Already downloaded image: " + link)
     else:
         try:
-            resp = urllib2.urlopen(link)
+            resp = urllib.request.urlopen(link,timeout=10)
             with open(dstPath, "wb") as f:
                 f.write(resp.read())
                 
@@ -20,9 +23,18 @@ def download_and_place(type, link, data_dir, index):
             #img = imread(dstPath)
             #assert(img is not None) # test if image was loaded correctly
             #print("Downloaded image {:4}: {}".format(index, link))
-        except:
+        except (URLError,HTTPError) as error:
+            print("URL {} not retrieved because {}".format(link,error))
             if os.path.exists(dstPath):
                 os.remove(dstPath)
+            return 0
+        except timeout:
+            print("Socket timed out - URL {}".format(link))
+            if os.path.exists(dstPath):
+                os.remove(dstPath)
+            return 0
+        except:
+            print("Unknown error.")
             return 0
     return 1
 
